57473bb7252af84b116ca49ac0d21cc8a5cec064
FrancieleAlmeida <francielytlc3@gmail.com>
2023-08-15 12:15:21 -0400

    aulas

---
+++ aula01/aula1-1.js
@@ -0,0 +1,30 @@
+//exercicio01
+console.log('exercicio01')
+var num = 10
+console.log(num)
+
+//exercicio02
+console.log('\n exercicio 02')
+var valor = 12
+
+if(valor % 2 == 0){
+    console.log(valor+' é par')
+}else{
+    console.log(valor+' é ímpar')
+}
+
+//exercicio03
+console.log('\nexercicio 03')
+var a = '20'
+var b = 20
+var c = 'vinte'
+
+if(a===b){
+    console.log('são iguais')
+}else if (a===b){
+    console.log('são iguais'+a+b)
+}else if(a==c){
+    console.log('são iguais'+a+b)
+}else{
+    console.log('são diferentes')
+}
---
+++ aula01/aula1.js
@@ -0,0 +1,38 @@
+var a =10
+var b = '20'
+var c = false
+
+let x = 20 // variavel local
+
+const y = 'fran' // isso é uma constante
+
+var h = a ==null
+
+var arr = new Array()
+
+var arr2 = new Array(1,2,3,4)
+
+var arr3 = [1,2,3,4,5]
+
+var arr4 = new Array (5)
+
+
+console.log(arr3)
+
+
+
+var d = (b===x) // é do tipo undefined
+var e = '' // none ou null
+
+console.log(d);
+
+if( a >= 10){
+    console.log(' a maior que 10')
+    let a = 30
+}else{
+    if (a<=20){
+        console.log('a menor que 20')
+    }
+}
+
+console.log(a)
